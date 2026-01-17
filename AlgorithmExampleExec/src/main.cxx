6939f7fecfa3b865dce6095a9394e436eec38967
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2026-01-17 12:23:18 -0400

    mejorar para compilar resolver misterios de porque C++ 20 es tan dificil de compilar

---
+++ AlgorithmExampleExec/src/main.cxx
@@ -0,0 +1,22 @@
+
+
+//import dinamic.factorial;
+
+int main(){
+    /*
+    int num = 5;
+
+    unsigned int result = Dinamic::Factorial(num);
+
+    std::cout << std::endl << "-- Factorial --" << std::endl << std::endl;
+
+    std::cout << "Factorial of " << num << " is " << result << std::endl;
+
+    if (result != 120) {
+        throw std::runtime_error("Error: División por cero.");
+    }
+
+    std::cout << std::endl << "Test Passed!" << std::endl;
+    */
+    return 0;
+}
